afad0713eb153a3e5fa65f2c92ac4f36e7e367b3
Betta_Fish <zxbmmmmmmmmm@qq.com>
2023-06-07 17:25:09 +0800

    优化主界面UI

---
+++ HyPlayer.Next.UISample/Helpers/Converters/BrushToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace HyPlayer.Next.UISample.Helpers.Converters;
+
+internal class BrushToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        var brushConverter = new ColorConverter();
+        var brush = (SolidColorBrush)value;
+        return brush.Color;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
+
